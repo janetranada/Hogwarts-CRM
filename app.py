ee0a7483dc4d8885fb8ae2f87a24bb3fc2fe95a9
janetranada <jyranada@gmail.com>
2020-01-17 21:44:25 +0200

    css styling; initial charts

--- app.py
+++ app.py
@@ -1,47 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import time
 import datetime
+from crm_data import student_list, magic, courses, houses
 
 app = Flask(__name__)
-
-
-student_list = {1: {'first_name': 'hermione',
-                    'last_name': 'ginger',
-                    'created': '2020-01-10 13:12:33',
-                    'last_updated': '2020-01-12 10:25:04',
-                    'current_magic': ['Alchemy 1', 'Possession 2', 'Invisibility 3', 'Invulnerability 4', 'Necromancer 5'],
-                    'desired_magic': ['Immortality 5', 'Elemental 4'],
-                    'desired_course': ['Alchemy Advanced', 'Magic for Day-to-Day Life'],
-                    },
-                2: {'first_name': 'harry',
-                    'last_name': 'potted',
-                    'created': '2019-11-10 03:12:33',
-                    'last_updated': '2020-01-12 16:20:41',
-                    'current_magic': ['Alchemy 1', 'Possession 4', 'Self-detonation 3', 'Summoning 2', 'Water breathing 1'],
-                    'desired_magic': ['Invisibility 5', 'Healing 4'],
-                    'desired_course': ['Alchemy Advanced', 'Dating with Magic'],
-                    },
-                3: {'first_name': 'ron',
-                    'last_name': 'whistle',
-                    'created': '2020-01-09 23:01:15',
-                    'last_updated': '2020-01-20 15:25:43',
-                    'current_magic': ['Immortality 2', 'Invulnerability 2', 'Necromancer 2', 'Omnipresent 2', 'Omniscient 2'],
-                    'desired_magic': ['Possession 3', 'Alchemy 5'],
-                    'desired_course': ['Alchemy Basics', 'Magic for Day-to-Day Life'],
-                    }
-                }
-
-
-magic = ['Alchemy', 'Animation', 'Conjuror', 'Disintegration', 'Elemental', 'Healing',
-         'Illusion', 'Immortality', 'Invisibility', 'Invulnerability', 'Necromancer',
-         'Omnipresent', 'Omniscient', 'Poison', 'Possession', 'Self-detonation',
-         'Summoning', 'Water breathing']
-
-courses = ['Alchemy Basics',
-           'Alchemy Advanced',
-           'Magic for Day-to-Day Life',
-           'Magic for Medical Professionals',
-           'Dating with Magic']
 
 
 def skills_list(status):
@@ -54,6 +16,20 @@ def skills_list(status):
     return skills
 
 
+def get_crm_skills_record(_target_magic):
+    global student_list
+    global magic
+    all_magic = []
+    for student in student_list:
+        student_magic_list = [magic.split()[0]
+                              for magic in student_list[student][_target_magic]]
+        all_magic.append(student_magic_list)
+
+    flatlist_magic = [item for sublist in all_magic for item in sublist]
+    skill_counter = [flatlist_magic.count(skill) for skill in magic]
+    return skill_counter
+
+
 @app.route("/")
 def landing_page():
     return render_template("landing_page.html")
@@ -61,35 +37,22 @@ def landing_page():
 
 @app.route("/get_data")
 def get_data():
-    global student_list
-    return jsonify(student_list)
+    data_to_pass = {}
+    current_skill_counter = get_crm_skills_record("current_magic")
+    desired_skill_counter = get_crm_skills_record("desired_magic")
+    data_to_pass["current_magic_counter"] = current_skill_counter
+    data_to_pass["desired_magic_counter"] = desired_skill_counter
+    data_to_pass["magic_name"] = magic
+    return jsonify(data_to_pass)
 
 
 @app.route("/home")
 def home_page():
-    return render_template("index.html")
+    return render_template("home.html")
 
 
 @app.route("/dashboard")
 def show_dashboard():
-    print('**********')
-    print('GET STATS')
-    all_list = []
-    for student in student_list:
-        the_list = [magic.split()[0]
-                    for magic in student_list[student]['current_magic']]
-        print(student_list[student]['first_name'] +
-              " " + student_list[student]['last_name'])
-        print(the_list)
-        all_list.append(the_list)
-    flat_list = [item for sublist in all_list for item in sublist]
-    curr_skill_counter = [flat_list.count(skill) for skill in magic]
-    print('counter:', curr_skill_counter)
-    print('possession>>', flat_list.count('Possession'))
-    print('the GIANT flat list>>', flat_list)
-    for i in range(0, len(magic)):
-        print(magic[i], '=', curr_skill_counter[i])
-    print("**********")
     return render_template("dashboard.html")
 
 
@@ -105,33 +68,55 @@ def find_student():
 
 @app.route("/student/<string:record_id>")
 def view_student(record_id):
-    return render_template("view_student.html", info=student_list[int(record_id)], rec_id=record_id)
+    return render_template("view_student.html",
+                           info=student_list[int(record_id)],
+                           rec_id=record_id)
 
 
 @app.route("/student/<string:record_id>/update", methods=['GET', 'POST'])
 def update_student(record_id):
     global student_list
     if request.method == 'GET':
-        return render_template('update_student.html', student_data=student_list[int(record_id)],  all_magic=magic, all_courses=courses, rec_id=record_id)
+        return render_template('update_student.html',
+                               student_data=student_list[int(record_id)],
+                               all_magic=magic,
+                               all_courses=courses,
+                               rec_id=record_id)
     else:  # method is POST
         curr_time = time.time()
         update_time = datetime.datetime.fromtimestamp(
             curr_time).strftime('%Y-%m-%d %H:%M:%S')
         desired_magic_list = skills_list('Desired')
-        current_magic_list = skills_list('Current')
+        all_current_magic = skills_list('Current')
         student_list[int(record_id)]['first_name'] = request.form["firstName"]
         student_list[int(record_id)]['last_name'] = request.form["lastName"]
         student_list[int(record_id)]['last_updated'] = update_time
-        student_list[int(record_id)]['current_magic'] = current_magic_list
+        student_list[int(record_id)]['current_magic'] = all_current_magic
         student_list[int(record_id)]['desired_magic'] = desired_magic_list
-        student_list[int(record_id)]['desired_course'] = request.form.getlist('courseDesired')
-        return render_template('update_student.html', student_data=student_list[int(record_id)],  all_magic=magic, all_courses=courses, rec_id=record_id, message='Record Updated')
+        student_list[int(record_id)]['desired_course'] = request.form.getlist(
+            'courseDesired')
+        return render_template('update_student.html',
+                               student_data=student_list[int(record_id)],
+                               all_magic=magic,
+                               all_courses=courses,
+                               rec_id=record_id,
+                               message='Record Updated')
 
 
 @app.route("/add", methods=['POST', 'GET'])
 def add_student():
     message = request.args.get('msg')
-    return render_template("add_student.html", message=message, all_magic=magic, all_courses=courses)
+    return render_template("add_student.html",
+                           message=message,
+                           all_magic=magic,
+                           all_courses=courses)
+
+
+@app.route("/student/<string:record_id>/delete")
+def delete_student(record_id):
+    print(student_list[int(record_id)])
+    del student_list[int(record_id)]
+    return redirect("/students")
 
 
 @app.route("/success", methods=['POST'])
@@ -143,19 +128,17 @@ def success_add():
     new_key = max(student_list.keys()) + 1
     new_student = {}
     desired_magic_list = skills_list('Desired')
-    current_magic_list = skills_list('Current')
+    all_current_magic = skills_list('Current')
     new_student['first_name'] = request.form["firstName"]
     new_student['last_name'] = request.form["lastName"]
     new_student['created'] = creation_time
     new_student['last_updated'] = creation_time
-    new_student['current_magic'] = current_magic_list
+    new_student['current_magic'] = all_current_magic
     new_student['desired_magic'] = desired_magic_list
     new_student['desired_course'] = request.form.getlist('courseDesired')
     student_list[new_key] = new_student
     return render_template('add_student.html', message="Record successfully added.")
-    # return redirect(url_for('view_student', record_id=new_key, info=student_list[new_key]))
-    # return render_template("success.html", content=student_list[new_key], rec_id=new_key, heading='This record was successfully added:')
-
+    
 
 @app.after_request
 def add_header(r):
--- static/crm.js
+++ static/crm.js
@@ -1,22 +1,86 @@
+function fetch_crm_data() {
+    fetch("/get_data")
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' + response.status);                
+                    return;
+                }
+                response.json().then(function (data) {
+                    console.log('success', data);
+                    create_charts(data)
+                })
+            })
+}
+
+
+function create_charts(_data){
+    magic_list = _data['magic_name']
+    current_magic_counter = _data["current_magic_counter"]
+    desired_magic_counter = _data["desired_magic_counter"]
+    curr_text = 'Current magic skills of Hogwarts students according to topic (since inception)'
+    desired_text = 'Desired magic skills of Hogwarts students according to topic (since inception)'
+    create_doughnut_Chart(magic_list, current_magic_counter, "Current Magic Skills", "currentMagicChart", curr_text);
+    create_doughnut_Chart(magic_list, desired_magic_counter, "Desired Magic Skills", "desiredMagicChart", desired_text);
+}
+
+
+function create_doughnut_Chart(_label, _data, _datasetLabel, _targetContainer, _text) {    
+    let ctx = document.getElementById(_targetContainer);
+    new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: _label,
+            datasets: [
+                {
+                    data: _data,
+                    label: _datasetLabel,
+                    backgroundColor: ['#f0134d', '#0c9463', '#f1fa3c', '#35495e',
+                        '#deff8b', '#ee8572', '#8cba51', '#655c56',
+                        '#ffcc00', '#c9485b', '#12cad6', '#697c37',
+                        '#eb8242', '#ff8ba7', '#c2b0c9', '#ff7315',
+                        '#f1d6ab', '#537ec5']
+                },
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: _text
+            },
+            legend: {
+                display: true,
+                position: 'right'
+            }
+        }
+    });
+}
+
 function createSelection(name, options, targetContainer) {
+    let select_container = document.createElement("div")
     let selection = document.createElement("select");
     selection.setAttribute("name", name);
     for (let i = 0; i < options.length; i++) {
         let option = document.createElement("option");
         option.setAttribute('value', options[i]);
         option.innerText = options[i];
-        selection.appendChild(option); 
+        selection.appendChild(option);
     }
-    targetContainer.appendChild(selection);
+    select_container.appendChild(selection)
+    targetContainer.appendChild(select_container);
 }
 
 const desiredMagicContainer = document.getElementById('desired-magic')
 const currentMagicContainer = document.getElementById('current-magic')
 const magic = ['Alchemy', 'Animation', 'Conjuror', 'Disintegration', 'Elemental',
-    'Healing', 'Illusion', 'Immortality', 'Invisibility', 'Invulnerability', 
-    'Necromancer', 'Omnipresent', 'Omniscient', 'Poison', 'Possession', 'Self-detonation', 
+    'Healing', 'Illusion', 'Immortality', 'Invisibility', 'Invulnerability',
+    'Necromancer', 'Omnipresent', 'Omniscient', 'Poison', 'Possession', 'Self-detonation',
     'Summoning', 'Water breathing']
-const courses = ['Alchemy basics', 'Alchemy advanced', 'Magic for day-to-day life', 'Magic for medical professionals', 'Dating with magic']
+const courses = ['Alchemy Basics',
+    'Alchemy Advanced',
+    'Magic for Day-to-Day Life',
+    'Magic for Medical Professionals',
+    'Dating with Magic']
 
 document.getElementById('desired-magic-btn').addEventListener('click', () => {
     console.log('desired magic')
@@ -30,5 +94,3 @@ document.getElementById('current-magic-btn').addEventListener('click', () => {
     createSelection(select_name, magic, desiredMagicContainer)
 })
 
-
-fetch()
